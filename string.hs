69283887ab465c9509af6975804e746d77d2a489
Ranjit Jhala <jhala@cs.ucsd.edu>
2017-06-14 21:18:42 -0700

    misc notes

--- string.hs
+++ string.hs
@@ -1,7 +1,7 @@
 {-@ LIQUID "--exact-data-con"  @-}
 {-@ LIQUID "--totality"        @-}
 {-@ LIQUID "--no-termination"  @-}
-{-@ LIQUID "--automatic-instances=liquidinstances" @-}
+{- LIQUID "--automatic-instances=liquidinstances" @-}
 {-@ LIQUID "--higherorder"                         @-}
 
 module String where
@@ -18,10 +18,21 @@ hasKey :: Key -> Dict a -> Bool
 hasKey k Emp         = False
 hasKey k (Key x v d) = k == x || hasKey k d
 
-{-@ get :: k:Key -> {d:Dict Int | hasKey k d} -> Int @-}
-get :: Key -> Dict Int -> Int
+{-@ lazy die @-}
+die :: a -> b
+die a = die a
+
+{-@ get :: k:String -> {d:Dict Int | hasKey k d} -> Int @-}
+get :: String -> Dict Int -> Int
 get k d1@(Key x v d)
   | k == x    = v
   | otherwise = liquidAssert (hasKey k d1) (get k d)
 get k Emp     = 0
 
+{-@ reflect tCatDog @-}
+tCatDog :: Dict a -> Bool
+tCatDog d = hasKey ("cat" :: String) d && hasKey ("dog" :: String) d
+
+{-@ d1 :: {d:Dict Int | tCatDog d} @-}
+d1 :: Dict Int
+d1 = Key "cat" 10 (Key "dog" 20 Emp)
